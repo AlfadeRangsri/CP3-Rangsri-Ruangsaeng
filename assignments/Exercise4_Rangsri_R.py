4e187612419a11670166d6e241c8fdcbe2f3d9f3
AlfredRangsri <Rangsri.rs@gmail.com>
2020-12-26 17:38:47 +0700

    Exercise4

---
+++ assignments/Exercise4_Rangsri_R.py
@@ -0,0 +1,10 @@
+fe = 78.2
+gb = 68.8
+ic = 80.5
+cp = 90.9
+
+print("--- Your Score ---")
+print("Foundation English :", fe)
+print("General Business :", gb)
+print("Introduction to Computer Systems :", ic)
+print("Computer Programming :", cp)
