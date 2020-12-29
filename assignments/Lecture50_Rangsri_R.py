8d89a12ec0e027faafa0f9f8bf1aec2b6687b0f6
AlfredRangsri <Rangsri.rs@gmail.com>
2020-12-30 06:30:12 +0700

    Lecture50

---
+++ assignments/Lecture50_Rangsri_R.py
@@ -0,0 +1,16 @@
+def addNumber(x, y):
+    print(x+y)
+
+def subtractNumber(x, y):
+    print(x-y)
+
+def multiplyNumber(x, y):
+    print(x*y)
+
+def divideNumber(x, y):
+    print(x/y)
+
+addNumber(10, 20)
+subtractNumber(10, 20)
+multiplyNumber(10, 20)
+divideNumber(20, 10)
