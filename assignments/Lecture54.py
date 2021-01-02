4727890905144111fd4d6e3e55ff5c250e61e90e
AlfredRangsri <Rangsri.rs@gmail.com>
2021-01-03 04:45:07 +0700

    Update Lecture54.py

--- assignments/Lecture54.py
+++ assignments/Lecture54.py
@@ -9,11 +9,13 @@ def login():
     else:
         return "Username or Password in correct."
 
+
 def showMenu():
     print("-----Alfred Shop-----")
     print("1. Vat Calculator")
     print("2. Price Calculator")
     menuSelect()
+
 
 def menuSelect():
     userSelected = int(input("Select menu : "))
@@ -23,14 +25,17 @@ def menuSelect():
     elif userSelected == 2:
         print(priceCalculate(), "Bath")
 
+
 def vatCalculate(totalPrice):
     vat = 7
-    result = totalPrice + (totalPrice*vat/100)
+    result = totalPrice + (totalPrice * vat / 100)
     return result
+
 
 def priceCalculate():
     price1 = int(input("First product price : "))
     price2 = int(input("Second product price : "))
-    return vatCalculate(price1+price2)
+    return vatCalculate(price1 + price2)
+
 
 login()
