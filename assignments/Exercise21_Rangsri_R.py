b371d5a72a707d345fc2a46fcdba34579a5732ce
AlfredRangsri <Rangsri.rs@gmail.com>
2021-01-06 07:55:51 +0700

    Exercise21

---
+++ assignments/Exercise21_Rangsri_R.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+def leftClickButton(event):
+    hight = float(textHight.get())
+    weight = float(textWeight.get())
+    bmi = weight / ((hight/100)**2)
+    if bmi > 30:
+        labelResult.configure(text="อ้วนมาก")
+    elif bmi >= 25:
+        labelResult.configure(text="อ้วน")
+    elif bmi >= 23:
+        labelResult.configure(text="น้ำหนักเกิน")
+    elif bmi >= 18.6:
+        labelResult.configure(text="เหมาะสม")
+    elif bmi < 18.6:
+        labelResult.configure(text="ผอมเกินไป")
+
+mainWindow = Tk()
+labelHight = Label(mainWindow, text='Height')
+labelHight.grid(row=0, column=0)
+textHight = Entry(mainWindow)
+textHight.grid(row=0, column=1)
+labelunit = Label(mainWindow, text='CM').grid(row=0, column=2)
+
+labelWeight = Label(mainWindow, text='Weight')
+labelWeight.grid(row=1, column=0)
+textWeight = Entry(mainWindow)
+textWeight.grid(row=1, column=1)
+labelunit2 = Label(mainWindow, text='KG').grid(row=1, column=2)
+
+calculateButton = Button(mainWindow, text='Calculate')
+calculateButton.grid(row=2, column=0)
+calculateButton.bind("<Button-1>", leftClickButton)
+
+labelResult = Label(mainWindow, text='BMI')
+labelResult.grid(row=2, column=1)
+mainWindow.mainloop()
