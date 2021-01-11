d56b3a8b8831cfd550ab937b5183244623de1658
AlfredRangsri <Rangsri.rs@gmail.com>
2021-01-12 05:58:16 +0700

    Update Lecture105_Rangsri_R.py

--- assignments/Lecture105_Rangsri_R.py
+++ assignments/Lecture105_Rangsri_R.py
@@ -7,29 +7,37 @@ class Vehicle:
 
 
 class Car(Vehicle):
-    pass
+    color = ""
 
 
 class Pickup(Vehicle):
-    pass
+    color = ""
 
 
 class Van(Vehicle):
-    pass
+    color = ""
 
 
 class EstateCar(Vehicle):
-    pass
+    color = ""
 
 
 car1 = Car()
+car1.color = 'Red'
+print("Car's color is :", car1.color)
 car1.turnonAirConditioner()
 
 pickUp1 = Pickup()
+pickUp1.color = 'Blue'
+print("Pickup's color is :", pickUp1.color)
 pickUp1.turnonAirConditioner()
 
 van1 = Van()
+van1.color = 'Yellow'
+print("Van's color is :", van1.color)
 van1.turnonAirConditioner()
 
 estateCar1 = EstateCar()
+estateCar1.color = 'Gray'
+print("EstateCar's color is :", estateCar1.color)
 estateCar1.turnonAirConditioner()
